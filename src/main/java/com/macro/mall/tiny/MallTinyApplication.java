60b1887940b1a344af533901160ed48668e34dc1
sunhui <1161186762@qq,com>
2019-11-15 16:59:03 +0800

    http://localhost:8080/swagger-ui.html#!/PmsBrandController/listBrandUsingGET

--- src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -1,5 +1,5 @@
 package com.macro.mall.tiny;
-//运行开始的地方！！
+//运行开始的地方！！接口地址：http://localhost:8080/swagger-ui.html
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
